36e943f0a3e6011dfca09ecf04ea835bb8ef0568
Mohamed Gamal Abdel Halim <mohamedgamalabdelhaliym@gmail.com>
2026-07-31 10:23:50 +0300

    refactor: migrate project to clean architecture

---
+++ enterprise-ecommerce.Application/Interfaces/Persistence/IProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using enterprise_ecommerce.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enterpriseecommerce.Application.Interfaces.Persistence
+{
+    public interface IProductRepository
+    {
+        Task<Product> AddAsync(Product product);
+
+        Task<List<Product>> GetAllAsync();
+
+        Task<Product?> GetByIdAsync(int id);
+
+        Task<Product?> UpdateAsync(Product product);
+
+        Task DeleteAsync(Product product);
+    }
+}
---
+++ enterprise-ecommerce.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,52 @@
+﻿using enterprise_ecommerce.Domain.Entities;
+using enterprise_ecommerce.Infrastructure.Persistence;
+using enterpriseecommerce.Application.Interfaces.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enterprise_ecommerce.Infrastructure.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly AppDbContext _db;
+
+        public ProductRepository(AppDbContext db)
+        {
+            this._db = db;
+        }
+        public async Task<Product> AddAsync(Product product)
+        {
+            await _db.Products.AddAsync(product);
+            await _db.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task DeleteAsync(Product product)
+        {
+            _db.Products.Remove(product);
+           await  _db.SaveChangesAsync();
+
+        }
+
+        public Task<List<Product>> GetAllAsync()
+        {
+            return _db.Products.ToListAsync();
+        }
+
+        public async Task<Product?> GetByIdAsync(int id)
+        {
+            return await _db.Products.FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<Product?> UpdateAsync(Product product)
+        {
+            _db.Products.Update(product);
+            await _db.SaveChangesAsync();
+            return product;
+        }
+    }
+}
